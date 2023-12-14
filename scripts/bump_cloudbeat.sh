e9d166366417a3318a9e7a159357e69f1674e6b4
Or Ouziel <or.ouziel@elastic.co>
2023-12-14 16:01:04 +0200

    foo

--- scripts/bump_cloudbeat.sh
+++ scripts/bump_cloudbeat.sh
@@ -60,13 +60,14 @@ create_cloudbeat_pr() {
     git checkout -b "$BRANCH" main
     git add .
     git commit -m "Bump cloudbeat to $NEXT_CLOUDBEAT_VERSION"
-    
-    echo "Create PR to bump cloudbeat version"
-    # gh auth login --with-token $GITHUB_CI_TOKEN
-    gh pr create --title "Bump cloudbeat" \
-             --body "Automated PR" \
-             --base "main" \
-             --head "$BRANCH"
+    git log
+    echo "wtf"
+    # echo "Create PR to bump cloudbeat version"
+    # # gh auth login --with-token $GITHUB_CI_TOKEN
+    # gh pr create --title "Bump cloudbeat" \
+    #          --body "Automated PR" \
+    #          --base "main" \
+    #          --head "$BRANCH"
 }
 
 bump_cloudbeat() {
