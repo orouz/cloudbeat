a583ddfe588cc6fb20f6d3bb2d882c66cfedf7bf
Or Ouziel <or.ouziel@elastic.co>
2023-12-14 18:03:45 +0200

    foo

--- scripts/bump_integration.sh
+++ scripts/bump_integration.sh
@@ -6,13 +6,18 @@
 # PR to integration repo to change kibana condition
 # PR to integration repo to change to preview version / production version
 
-export MANIFEST_PATH="packages/cloud_security_posture/manifest.yml"
+MANIFEST_PATH="packages/cloud_security_posture/manifest.yml"
+
+checkout_integration_repo() {
+    git clone git@github.com:orouz/integrations.git
+    cd integrations
+}
 
 replace_manifest_version_vars() {
-    MINOR_VERSION=$(echo $VERSION | cut -d '.' -f1,2)
+    MINOR_VERSION=$(echo $NEXT_CLOUDBEAT_VERSION | cut -d '.' -f1,2)
     echo "MINOR_VERSION is $MINOR_VERSION"
 
-    PATCH_VERSION=$VERSION
+    PATCH_VERSION=$NEXT_CLOUDBEAT_VERSION
     echo "PATCH_VERSION is $PATCH_VERSION"
 
     # cis_gcp
@@ -23,30 +28,24 @@ replace_manifest_version_vars() {
     sed -i'' -E "s/cloudformation-cspm-ACCOUNT_TYPE-[0-9]+\.[0-9]+\.[0-9]+/cloudformation-cspm-ACCOUNT_TYPE-$PATCH_VERSION/g" $MANIFEST_PATH
 
     # cis_azure
-    sed -i'' -E "s/cloudbeat%2F[0-9]+\.[0-9]+/cloudbeat%2F$PATCH_VERSION/g" $MANIFEST_PATH
+    sed -i'' -E "s/cloudbeat%2F[0-9]+\.[0-9]+/cloudbeat%2F$MINOR_VERSION/g" $MANIFEST_PATH
 }
 
 create_integrations_pr() {
-    local BRANCH="bump-to-$CLOUDBEAT_VERSION"
+    local BRANCH="bump-to-$NEXT_CLOUDBEAT_VERSION"
     git config --global user.email "elasticmachine@users.noreply.github.com"
     git config --global user.name "Elastic Machine"
     git checkout -b "$BRANCH" main
     git add $MANIFEST_PATH
-    git commit -m "Bump integration manifest to $CLOUDBEAT_VERSION"
+    git commit -m "Bump integration manifest to $NEXT_CLOUDBEAT_VERSION"
     git push origin $BRANCH
-    gh auth login --with-token $GITHUB_CI_TOKEN
+    
     gh pr create --title "[Cloud Security] Update integration manifest" \
-             --body "Automated PR" \
-             --base "main" \
-             --head "$BRANCH"
+  --body "Automated PR" \
+  --base "main" \
+  --head "$BRANCH"
 }
 
+checkout_integration_repo
 replace_manifest_version_vars
 create_integrations_pr
-
-# bump_integration_version() { 
-#     # NEXT_INTEGRATION_VERSION
-#     # update manifest
-#     # add change log entry 
-# }
-
