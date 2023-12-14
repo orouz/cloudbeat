7e6d605ad056e763d437e3d1d7a194276f89a1a2
Or Ouziel <or.ouziel@elastic.co>
2023-12-14 16:13:21 +0200

    foo

--- scripts/bump_cloudbeat.sh
+++ scripts/bump_cloudbeat.sh
@@ -60,7 +60,7 @@ create_cloudbeat_pr() {
     git add .
     git commit -m "Bump cloudbeat to $NEXT_CLOUDBEAT_VERSION"
     git push origin "$BRANCH"
-    
+
     # echo "Create PR to bump cloudbeat version"
     # # gh auth login --with-token $GITHUB_CI_TOKEN
     gh pr create --title "Bump" \
@@ -71,8 +71,8 @@ create_cloudbeat_pr() {
 
 bump_cloudbeat() {
     update_version_mergify
-    # update_version_arm_template
-    # update_version_beat
+    update_version_arm_template
+    update_version_beat
 }
 
 # create_release_branch
